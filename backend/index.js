dd6cf7e38067b93dc81b72c688497d6f1599d662
srimant0203 <srimant.acharjya@gmail.com>
2025-04-09 20:26:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-import path from path;
+import path from "path";
 
 dotenv.config({});
 
